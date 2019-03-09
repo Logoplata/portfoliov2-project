7137520c73e6b22cd9ad479ab073a398bdaf4f2f
Logan <Lhugli6386@hotmail.com>
2019-03-09 12:11:54 -0500

    Button updates started portfolio functionality

--- portfoliov2/urls.py
+++ portfoliov2/urls.py
@@ -23,5 +23,5 @@ import projects.views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home.views.home, name='home'),
-    path('blog/', include('blog.urls'))
+    path('blog/', include('blog.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- projects/views.py
+++ projects/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
 
+from .models import Project
+
 def projects(request):
-	return render(request, 'projects.html')
+	return render(request, 'projects/projects.html')
