49f4a9153ecacf1fe59ec7470e656035c9de3c2a
Logan <Lhugli6386@hotmail.com>
2019-03-14 18:20:20 -0400

    Final commit

--- projects/views.py
+++ projects/views.py
@@ -3,4 +3,5 @@ from django.shortcuts import render
 from .models import Project
 
 def allprojects(request):
-	return render(request, 'projects/projects.html')
+	projects = Project.objects
+	return render(request, 'projects/projects.html', {'projects':projects})
